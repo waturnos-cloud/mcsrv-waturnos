6da7a61e89909017b989908d6a1db7331699bb0d
svillanueva <svillanueva@192.168.0.124>
2025-10-27 12:07:02 -0300

    Ajustes

--- src/main/java/com/waturnos/config/AppConfig.java
+++ src/main/java/com/waturnos/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.waturnos.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 import com.waturnos.security.CustomUserDetailsService;
 import com.waturnos.security.JwtAuthFilter;
@@ -26,10 +29,26 @@ public class AppConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/login", "/user/register","/auth/logingoogle").permitAll()
-						.anyRequest().authenticated())
+				
 				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.cors(cors -> cors.configurationSource(request -> {
+					CorsConfiguration config = new CorsConfiguration();
+					config.setAllowedOrigins(List.of("http://localhost:5173", // Front local
+							"http://localhost:5174", // Alternativo (si usás otro puerto)
+							"https://waturnos-admin.vercel.app" // Front desplegado
+					));
+					config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+					config.setAllowedHeaders(
+							List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
+					config.setExposedHeaders(List.of("Authorization"));
+					config.setAllowCredentials(true);
+					return config;
+				})).sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", "/swagger-ui.html",
+						"/swagger-ui/**", "/api-docs/**", "/v3/api-docs/**").permitAll().anyRequest().authenticated())
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+				
+				
 		return http.build();
 	}
     @Bean
