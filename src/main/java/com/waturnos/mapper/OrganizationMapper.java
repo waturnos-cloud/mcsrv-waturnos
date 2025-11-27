63549505cb13f330d0b48d7be7cfcd10136a7801
nbacci <nicolas.bacci@gmail.com>
2025-11-27 19:41:22 -0300

    Mejoras performance listado de organizaciones

--- src/main/java/com/waturnos/mapper/OrganizationMapper.java
+++ src/main/java/com/waturnos/mapper/OrganizationMapper.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import com.waturnos.dto.beans.LocationDTO;
 import com.waturnos.dto.beans.OrganizationDTO;
+import com.waturnos.dto.beans.CategoryTreeDTO;
 import com.waturnos.entity.Location;
 import com.waturnos.entity.Organization;
+import com.waturnos.entity.Category;
 
 @Mapper(componentModel = "spring")
 public abstract class OrganizationMapper {
@@ -21,12 +24,17 @@ public abstract class OrganizationMapper {
         }
         return dto;
     }
-	@Mapping(target = "locations", ignore = true) 
-	public abstract OrganizationDTO mapBasicInfo(Organization entity);
+    @Mapping(target = "locations", ignore = true)
+    @Mapping(target = "type", source = "type", qualifiedByName = "categoryWithoutChildren")
+    public abstract OrganizationDTO mapBasicInfo(Organization entity);
 	
 	public abstract List<LocationDTO> mapLocations(List<Location> subBean);
 	
 	public abstract List<Location> mapLocationsToEntity(List<LocationDTO> subBean);
 
-	public abstract Organization toEntity(OrganizationDTO dto);
+    public abstract Organization toEntity(OrganizationDTO dto);
+
+    @Named("categoryWithoutChildren")
+    @Mapping(target = "children", ignore = true)
+    protected abstract CategoryTreeDTO toCategoryTreeWithoutChildren(Category category);
 }
